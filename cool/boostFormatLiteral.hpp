9996eb818f6235b1f1550196aa71ff6def64322f
light_and_ray <light.and.ray@gmail.com>
2021-04-23 02:23:33 +0300

    Added boostFormatLiteral.hpp

---
+++ cool/boostFormatLiteral.hpp
@@ -0,0 +1,20 @@
+#ifndef COOL_BOOSTFORMATLITERAL_HPP
+#define COOL_BOOSTFORMATLITERAL_HPP
+
+#include <boost/format.hpp>
+
+namespace cool
+{
+	namespace boost
+	{
+		namespace literals
+		{
+			::boost::format operator ""_f(const char* cstr, size_t size)
+			{
+				return ::boost::format(cstr);
+			}
+		}
+	}
+}
+
+#endif
