ec2bd771e36645a04aad137d7d026059ebece0b3
Andray <light.and.ray@gmail.com>
2022-01-11 22:32:50 +0300

    init random seed

--- cool/random.hpp
+++ cool/random.hpp
@@ -6,29 +6,40 @@
 
 namespace cool
 {
-	//from rmin to rmin inclusive 
+	void _init()
+	{
+		static bool initialized = false;
+		if (!initialized){
+			srand(time(NULL));
+			initialized = true;
+		}
+	}
+
+	//from rmin to rmin inclusive
 	double random(double rmin, double rmax, int precision = 4)
 	{
+		_init();
 		int x = rand();
 		double y = x * (rmax - rmin + 1) / RAND_MAX + rmin;
 		return round(y * pow(10, precision)) / pow(10, precision);
 	}
 
-	//from 0 to rmin inclusive 
+	//from 0 to rmin inclusive
 	inline double random(double rmax, int precision = 4)
 	{
 		return random(0, rmax, precision);
 	}
 
-	//from rmin to rmin inclusive 
+	//from rmin to rmin inclusive
 	int random(int rmin, int rmax)
 	{
+		_init();
 		int x = rand();
 		int y = ((long)x * (rmax - rmin + 1)) / RAND_MAX + rmin;
 		return y;
 	}
 
-	//from 0 to rmin inclusive 
+	//from 0 to rmin inclusive
 	inline int random(int rmax)
 	{
 		return random(0, rmax);
